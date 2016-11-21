2cd91cc9202c7934be224283276f618a8bc99cd9
Marissa Nur Aini <marissarissarissa19@gmail.com>
2016-11-21 21:41:48 +0700

    Marissa commit1

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl308172635/my_list/InputTugas.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl308172635/my_list/InputTugas.java
@@ -16,5 +16,7 @@ public class InputTugas extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.inputtugas, container, false);
+
+
     }
 }
